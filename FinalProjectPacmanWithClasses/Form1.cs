c6e176f63ece1fbe5982ab72babc3d2c5d416506
tobechuk.okoro <okoroc333@gmail.com>
2024-03-13 11:54:56 -0600

    Add project files.

---
+++ FinalProjectPacmanWithClasses/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjectPacmanWithClasses
+{
+    public partial class Form1 : Form
+    {
+        private Wall wall1;
+        private Wall wall2;
+        private Wall wall3;
+        private Wall wall4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            this.Width = 1700;
+            this.Height = 1000;
+
+            wall1 = new Wall(400, 0, this);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            wall1.Draw(e.Graphics);
+        }
+    }
+}
---
+++ FinalProjectPacmanWithClasses/Wall.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjectPacmanWithClasses
+{
+    internal class Wall
+    {
+        private Form form;
+        private int xLoc;
+        private int yLoc;
+        private int width;
+        private int height;
+        private Pen pen;
+        private Random rand = new Random();
+        private int xSpeed = -10;
+        private int ySpeed = 0;
+
+        public int X { get { return xLoc; } }
+        public int Y { get { return yLoc; } }
+        public int Width { get { return width; } }
+        public int Height { get { return height; } }
+
+        public Wall (int x, int y, Form mainform)
+        {
+            xLoc = x;
+            yLoc = y;
+            width = 30;
+            height = 200;
+            pen = new Pen(Color.Blue);
+            form = mainform;
+        }
+
+        public void Draw(Graphics gr)
+        {
+            //gr.DrawEllipse(pen, xLoc, yLoc, width, height);
+            gr.DrawRectangle(pen, xLoc, yLoc, width, height);
+
+            //xLoc += xSpeed;
+            //yLoc += ySpeed;
+            //if (xLoc + width >= form.ClientSize.Width) xSpeed *= -1;
+            //if (xLoc <= 0) xSpeed *= -1;
+            //if (yLoc + height >= form.ClientSize.Height) ySpeed *= -1;
+            //if (yLoc <= 0) ySpeed *= -1;
+        }
+    }
+}
